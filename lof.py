b62754a0e9cae6592696e480c5f83d961a9cc1d1
mtngt <mtngt@benjamins-air.lan>
2018-11-08 22:38:57 -0500

    yeet

--- lof.py
+++ lof.py
@@ -1,6 +1,9 @@
 from itertools import combinations
 from math import sqrt
 from json import dumps
+from collections import OrderedDict
+from operator import itemgetter
+
 
 class LOF:
 
@@ -20,6 +23,8 @@ class LOF:
             self.write_distance(combo[0], combo[1], distance)
             self.write_distance(combo[1], combo[0], distance)
 
+
+
         # calculate each ones kth nearest
 
         print(dumps(self.coordinates, indent=4, sort_keys=True))
@@ -28,28 +33,28 @@ class LOF:
             print(key)
             sorted_list = []
             for sub_key, sub_value in self.coordinates[key]['distances'].items():
-                print(sub_key)
-                print(sub_value)
+                print('-' + str(sub_key))
+                print('-' + str(sub_value))
 
         # TODO: we actually dont need to store an item if it is over the kth item distance away
         # we can discard it and not have to sort though it.
         # TODO: we can actiually instantiate an array of length k and just populate that as items come in
         # and sort it in place
 
-
-            "a": {
-                "distances": {
-                    "b": 3,
-                    "c": 7,
-                    "d": 11
-                },
-                "distances": [
-                    "b": 3,
-                    "c": 7,
-                    "d": 11] ,
-                "x": 0,
-                "y": 0
-            }
+            #
+            # "a": {
+            #     "distances": {
+            #         "b": 3,
+            #         "c": 7,
+            #         "d": 11
+            #     },
+            #     "distances": [
+            #         "b": 3,
+            #         "c": 7,
+            #         "d": 11] ,
+            #     "x": 0,
+            #     "y": 0
+            # }
 
 
             # self.coordinates[[combo[0]]['dists'][combo[1]]] = dist
@@ -61,7 +66,25 @@ class LOF:
 
     def write_distance(self, key_1, key_2, distance):
         if 'distances' in self.coordinates[key_1]:
+            print('Total Items in Dict: ' + str(len(self.coordinates[key_1]['distances'])))
+            if len(self.coordinates[key_1]['distances']) == self.k:
+                print('could already be full')
+                print(self.coordinates[key_1]['distances'][self.coordinates[key_1]['distances'].keys()[0]])
+                if distance > self.coordinates[key_1]['distances'][self.coordinates[key_1]['distances'].keys()[0]]:
+                    return
+                else:
+                    return
+
+
             self.coordinates[key_1]['distances'][key_2] = distance
+            print('before')
+            print(self.coordinates[key_1]['distances'])
+            print('after')
+            self.coordinates[key_1]['distances'] = \
+                dict(OrderedDict(sorted(self.coordinates[key_1]['distances'].items(),
+                                        key=itemgetter(1),
+                                        reverse=True)))
+            print(self.coordinates[key_1]['distances'])
         else:
             self.coordinates[key_1]['distances'] = {}
             self.coordinates[key_1]['distances'][key_2] = distance
@@ -100,6 +123,19 @@ coords = {
 }
 
 lof = LOF(coords, 'hello', 2)
+
+# dicty =  {
+#     "distances": {
+#         "b": 3,
+#         "c": 15,
+#         "d": 11
+#     }
+# }
+# temp_dict = dicty['distances']
+# print(temp_dict)
+#
+# print(dict(OrderedDict(sorted(temp_dict.items(), key=itemgetter(1), reverse=False))))
+
 
 
 # DIFFERENT WAYS TO REPRESENT DATA
